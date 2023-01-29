da2c4a98a918f1f2a3e3b41930ece29f3ce740f0
chickenchickenlove <ojt90902@naver.com>
2023-01-29 12:37:46 +0900

    src/test/java/hello/springtx/apply/InternalCallV2Test.java

---
+++ src/test/java/hello/springtx/apply/InternalCallV2Test.java
@@ -0,0 +1,71 @@
+package hello.springtx.apply;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+@Slf4j
+public class InternalCallV2Test {
+
+    @Autowired
+    CallService callService;
+
+
+    @Test
+    void externalCall() {
+        // AOP 적용됨.
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1Config {
+        @Bean
+        public CallService callService() {
+            return new CallService(internalService());
+        }
+
+        @Bean
+        public InternalService internalService() {
+            return new InternalService();
+        }
+    }
+
+    @RequiredArgsConstructor
+    static class CallService {
+
+        private final InternalService service;
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            service.internal();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+    }
+
+    static class InternalService {
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+    }
+
+}
