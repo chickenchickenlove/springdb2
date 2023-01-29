12d83764bd2c1e956f1b6d40084693812044d1c8
chickenchickenlove <ojt90902@naver.com>
2023-01-29 11:36:08 +0900

    TxApplyBasicTest

--- src/test/java/hello/springtx/apply/TxApplyBasicTest.java
+++ src/test/java/hello/springtx/apply/TxApplyBasicTest.java
@@ -1,2 +1,67 @@
-package hello.springtx.apply;public class TxApplyBasicTest {
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+public class TxApplyBasicTest {
+
+    @Autowired
+    BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class = {}", basicService.getClass());
+        basicService.tx();
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.noTx();
+    }
+
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+
+        @Bean
+        public BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+
+        public void noTx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+
+
+
+
+    }
+
 }
