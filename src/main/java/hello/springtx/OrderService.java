90f6578f44eff3e49d971f36b28d66057cbb778b
chickenchickenlove <ojt90902@naver.com>
2023-01-31 17:23:37 +0900

    h

--- src/main/java/hello/springtx/OrderService.java
+++ src/main/java/hello/springtx/OrderService.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
